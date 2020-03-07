fe3563c38b90b1aa1dd505e3f7768e8ec87e2071
AnatRotem1 <31000267+AnatRotem1@users.noreply.github.com>
2020-03-07 20:19:41 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -37,3 +37,37 @@
 //       log.snapshot().end()
 //     })
 // })
+
+
+//A function to expand Node 1
+Cypress.Commands.add('expandNode1', () => {
+    cy.get('.DropdownTree').click()
+    cy.get('.TreeNodeExpandIcon > .SvgIcon').click()
+})
+
+
+//A function to expand Node 2
+Cypress.Commands.add('expandNode2', () => {
+    cy.get('.DropdownTree').click()
+    cy.get('.TreeNodeExpandIcon > .SvgIcon').click()
+    cy.get('[data-testid=TreeSelection__node-header-node2] > .TreeNode > .TreeNode__inner > .TreeNode__main > .TreeNodeExpandIcon > .SvgIcon > use').click()
+})
+
+
+//A function to select a node from the tree and assert that expectedText is displayed in the text bar, isChildOfNode2 === true in case the node is child of Node 2
+Cypress.Commands.add('SelectNode', (node, expectedText, isChildOfNode2) => {
+    
+    cy.visit('http://storybook.b360-dev.autodesk.com/current/iframe.html?id=dropdowntree--default');
+
+    if (isChildOfNode2 === true){
+       cy.expandNode2()
+
+    } else {
+        cy.expandNode1()
+    }
+        cy.contains(node).click()
+        cy.get('.DropdownTree__input-text > span').should('contain', expectedText) 
+})
+
+
+
